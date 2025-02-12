286283b4f99d64aca4ff610e3e4488e07b34350a
Sage Elliott <hello@sageelliott.com>
2025-02-12 10:27:03 -0800

    add bacth inference to tutorial

--- tasks/inference.py
+++ tasks/inference.py
@@ -1,7 +1,12 @@
 from flytekit import task, Resources, workflow
 from flytekit.types.directory import FlyteDirectory
-from containers import container_image
+from containers import container_image, actor
+import union
 
+
+# ---------------------------
+# Batch inference with Regular Containers
+# ---------------------------
 @task(
     container_image=container_image,
     requests=Resources(cpu="2", mem="2Gi", gpu="1"),
@@ -22,3 +27,31 @@ def predict_batch_sentiment(trained_model_dir: FlyteDirectory, texts: list[str])
     predictions = nlp_pipeline(texts, batch_size=8)
 
     return predictions
+
+# ---------------------------
+# Batch inference with Union Actors
+# ---------------------------
+
+# @union.actor_cache
+# def load_model(trained_model_dir: FlyteDirectory) -> KNeighborsClassifier:
+#     from transformers import AutoTokenizer, AutoModelForSequenceClassification, pipeline
+
+#     # actor caching is useful for large models
+#     model_dir = trained_model_dir.download()
+#     model = AutoModelForSequenceClassification.from_pretrained(model_dir)
+#     tokenizer = AutoTokenizer.from_pretrained(model_dir)
+
+#     # Initialize the pipeline for sentiment analysis
+#     nlp_pipeline = pipeline("text-classification", model=model, tokenizer=tokenizer)
+    
+#     return nlp_pipeline
+
+# @actor.task
+# def actor_model_predict(
+#    trained_model_dir: FlyteDirectory, texts: list[str]
+# ) -> List[int]:
+#     nlp_pipeline = load_model(trained_model_dir)
+
+#     predictions = nlp_pipeline(texts, batch_size=8)
+
+#     return predictions.tolist()
--- workflows/batch_inference.py
+++ workflows/batch_inference.py
@@ -1,13 +1,20 @@
-from flytekit import task, Resources, workflow
+from flytekit import task, Resources, workflow, Artifact
 from flytekit.types.directory import FlyteDirectory
 from containers import container_image
+from tasks.inference import predict_batch_sentiment
 
+FineTunedImdbModel = Artifact(name="fine_tuned_Imdb_model")
 
 # ---------------------------
 # Batch inference with regular containers
 # ---------------------------
-
-
+@workflow
+def batch_inference_workflow(texts: list[str],
+                              trained_model_dir: FlyteDirectory = FineTunedImdbModel.query()
+                              ) -> list[dict]:
+    # predict_batch_sentiment(trained_model_dir=trained_model_dir, texts=texts)
+    pred = predict_batch_sentiment(trained_model_dir=trained_model_dir, texts=texts)
+    return pred
 
 
 # ---------------------------
