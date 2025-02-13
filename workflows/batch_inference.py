be57770c1bf1acec5a4206368bd61193a49147f5
Sage Elliott <hello@sageelliott.com>
2025-02-13 12:28:43 -0800

    clean up code formatting on workflows

--- workflows/batch_inference.py
+++ workflows/batch_inference.py
@@ -1,17 +1,18 @@
-from flytekit import task, Resources, workflow, Artifact
+from flytekit import Artifact, workflow
 from flytekit.types.directory import FlyteDirectory
-from containers import container_image
-from tasks.inference import predict_batch_sentiment, actor_model_predict
+
+from tasks.inference import actor_model_predict, predict_batch_sentiment
 
 FineTunedImdbModel = Artifact(name="fine_tuned_Imdb_model")
+
 
 # ---------------------------
 # Batch inference with regular containers
 # ---------------------------
 @workflow
-def batch_inference_workflow(texts: list[str],
-                              trained_model_dir: FlyteDirectory = FineTunedImdbModel.query()
-                              ) -> list[dict]:
+def batch_inference_workflow(
+    texts: list[str], trained_model_dir: FlyteDirectory = FineTunedImdbModel.query()
+) -> list[dict]:
     # predict_batch_sentiment(trained_model_dir=trained_model_dir, texts=texts)
     pred = predict_batch_sentiment(trained_model_dir=trained_model_dir, texts=texts)
     return pred
@@ -21,8 +22,8 @@ def batch_inference_workflow(texts: list[str],
 # Faster Batch inference with Actors
 # ---------------------------
 @workflow
-def actor_batch_inference_workflow(texts: list[str],
-                              trained_model_dir: FlyteDirectory = FineTunedImdbModel.query()
-                              ) -> list[dict]:
+def actor_batch_inference_workflow(
+    texts: list[str], trained_model_dir: FlyteDirectory = FineTunedImdbModel.query()
+) -> list[dict]:
     pred = actor_model_predict(trained_model_dir=trained_model_dir, texts=texts)
     return pred
--- workflows/train_pipeline.py
+++ workflows/train_pipeline.py
@@ -1,45 +1,47 @@
-'''
+"""
 This file contains the train_pipeline workflow that orchestrates the
 training pipeline for BERT classification models
-'''
+"""
 
 from union import workflow
+
 from tasks.data import download_dataset, visualize_data
-from tasks.model import download_model, train_model, evaluate_model
 from tasks.inference import predict_batch_sentiment
+from tasks.model import download_model, evaluate_model, train_model
+
 
 # ---------------------------
 # train pipeline
 # ---------------------------
 @workflow
-def train_pipeline(model_name: str = "bert-base-uncased",
-                    epochs: int = 3,
-                    extra_test_text: list[str] = [
-                        "This is a great movie!",
-                        "This is a bad movie!",
-    ]) -> None:
+def train_pipeline(
+    model_name: str = "bert-base-uncased",
+    epochs: int = 3,
+    extra_test_text: list[str] = [
+        "This is a great movie!",
+        "This is a bad movie!",
+    ],
+) -> None:
 
     train_dataset, val_dataset, test_dataset = download_dataset()
     saved_model_dir = download_model(model_name=model_name)
-    
-    visualize_data(train_dataset=train_dataset, val_dataset=val_dataset, test_dataset=test_dataset)
+
+    visualize_data(
+        train_dataset=train_dataset, val_dataset=val_dataset, test_dataset=test_dataset
+    )
 
     trained_model_dir = train_model(
         model_dir=saved_model_dir,
         train_dataset=train_dataset,
         val_dataset=val_dataset,
-        epochs=epochs
+        epochs=epochs,
     )
 
-    evaluate_model(
-        trained_model_dir=trained_model_dir,
-        test_dataset=test_dataset
-    )
+    evaluate_model(trained_model_dir=trained_model_dir, test_dataset=test_dataset)
 
     # Perform batch inference
     predict_batch_sentiment(trained_model_dir=trained_model_dir, texts=extra_test_text)
 
-    
 
 # Run model training pipeline:
 #!union run --remote workflows/train_pipeline.py train_pipeline
